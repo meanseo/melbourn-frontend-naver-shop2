247c49a7459d790a33559ea48d0f6b9034c9f79d
meanseo <chlalstjok97@gmail.com>
2022-02-24 19:46:00 +0900

    연결

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,33 +1,49 @@
 import React, { useState } from "react";
 import Layout from "../containers/Layout";
+import axios from "axios";
+import { useLinkClickHandler } from "react-router-dom";
 
 export default function Bmi(){
 
-    const [username, setUsername] = useState("")
-    const [height, setHeight] = useState(0.0)
-    const [weight, setWeight] = useState(0.0)
-    const res = () => {
-        let username = document.getElementById('username').value
-        let height = document.getElementById('height').value
-        let weight = document.getElementById('weight').value
-        setUsername(username)
-        setHeight(height)
-        setWeight(weight)
+    const [inputs, setInputs] = useState({})
+    const { name, weight, height } = inputs; // Object Destructuring
+
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const {value, name} = e.target;
+        setInputs({...inputs, [name]: value})
     }
     
+    const handleClick = (e) => {
+        e.preventDefault()
+        const bmiRequest = {name, height, weight}
+        alert(`사용자 이름: ${JSON.stringify(bmiRequest)}`)
+        /**
+        axios.get(`http://localhost:8080/member/bmi`)
+           .then((res) => {
+               alert(`답장이 도착했습니다.[내용] ${JSON.stringify(res.data)}`)
+           })*/
+    
+    }
 
     return(<Layout>
+        <form>
         <h1>BMI</h1>
+
         <div>
-        <label><b>Username</b></label>
-        <input id="username" /><br/>
-        <label><b>Height</b></label>
-        <input id="height" /><br/>
-        <label><b>Weight</b></label>
-        <input id="weight" /><br/>
-        <button onClick={()=>{res()}}>execute</button>
-        <div>결과 : {username} {height} {weight}</div>
+        <label><b>name</b></label>
+        <input type="text" name ="name" onChange={handleChange} /><br/>
+
+        <label htmlFor=""><b>Height</b></label>
+        <input type="text" name="height" onChange={handleChange} /><br/>
+
+        <label htmlFor=""><b>Weight</b></label>
+        <input type="text" name="weight" onChange={handleChange} /><br/>
+            <button onClick={handleClick}>BMI 체크</button>
         </div>
+        </form>
+        
         </Layout>
 
     )
--- components/Grade.js
+++ components/Grade.js
@@ -2,35 +2,44 @@ import React, { useState } from "react";
 import Layout from "../containers/Layout";
 
 export default function Grade (){
-        const [name,setName] = useState("")
-        const [kor,setKor] = useState(0)
-        const [eng,setEng] = useState(0)
-        const [math,setMath] = useState(0)
-        const res =()=>{
-            let name = document.getElementById('name').value
-            let kor = document.getElementById('kor').value
-            let eng = document.getElementById('eng').value
-            let math = document.getElementById('math').value
-            setName(name)
-            setKor(kor)
-            setEng(eng)
-            setMath(math)
-        }
+        
+    const [inputs, setInputs] = useState({})
+    const { name, kor, eng, math } = inputs;
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        const {value, name} = e.target;
+        setInputs({...inputs, [name]: value})
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        const calcRequest = {name, kor, eng, math}
+        alert(`사용자 이름: ${JSON.stringify(calcRequest)}`)
+    }
+
 
     return(<Layout>
+        <form>
         <h1>성적표</h1>
+
         <div>
         <label><b>이름</b></label>
-        <input id="name"/><br/>
-        <label><b>국어 점수</b></label>
-        <input id="kor"/><br/>
+        <input type="text" name ="name" onChange={handleChange}/><br/>
+
+        <label htmlFor=""><b>국어 점수</b></label>
+        <input type="text" name="kor" onChange={handleChange}/><br/>
+
         <label><b>영어 점수</b></label>
-        <input id="eng"/><br/>
+        <input type="" name="eng"/><br/>
+
         <label><b>수학 점수</b></label>
-        <input id="math"/><br/>
-        <button onClick={()=>{res()}}>execute</button>
-        <div>결과: {name} {kor} {eng} {math}</div>
+        <input type="" name="math"/><br/>
+
+        <button onClick={handleClick}>execute</button>
+        
         </div>
+        </form>
         
         </Layout>
 
